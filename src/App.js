99ccaef7545f44114001e107058e2db613fe0604
shivam-sharma0 <vashistshivam931@gmail.com>
2023-04-21 12:34:38 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+const [people, setpeople] = useState(data);
+ 
+  return <main>
+   <section className='container'>
+    <h3>{people.length} Birthday's</h3>
+    <List people={people}></List>
+    <button onClick={()=>setpeople([])} >
+      Clear All
+    </button>
+   </section>
+  </main>;
 }
 
 export default App;
